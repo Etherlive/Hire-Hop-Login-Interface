97bfb155daf062a8178da13f487ba6f69072baa9
Oscar Davies <odavies@etherlive.co.uk>
2021-09-03 15:57:34 +0100

    Connection Identities

--- HireHop-Login-Interface/Controllers/LoginController.cs
+++ HireHop-Login-Interface/Controllers/LoginController.cs
@@ -11,29 +11,41 @@ namespace HireHop_Login_Interface.Controllers
     [Route("auth")]
     public class LoginController : Controller
     {
-        // POST: LoginController/Create
-        [HttpPost("login")]
-        public async Task<ActionResult> Create([FromHeader]string username, [FromHeader]string password, [FromHeader]string companyId)
+        [HttpGet("trackme")]
+        public ActionResult TrackMe()
         {
-            if (username == null || password==null|| username.Length<1 || password.Length < 1)
-            {
-                return Json(new { status = "error", message = "Invalid Username or Password length" });
-            }
-            ClientConnection conn;
+            string identifier = Services.ConnectionManager.CreateClient();
+            Response.Cookies.Append("identity", identifier);
+            return Json(new { identity = identifier });
+        }
 
-            if (companyId == null) conn = await Services.Authentication.Login(new Services.ClientConnection(), username, password);
-            else conn = await Services.Authentication.Login(new Services.ClientConnection(), username, password, companyId);
-
-            bool successful_login = conn.cookies.Count > 0 && !conn.cookies.Any(x => x.Name == "id" && x.Value == "deleted");
-
-            if (successful_login)
+        // POST: auth/login
+        [HttpPost("login")]
+        public async Task<ActionResult> Create([FromHeader] string identity, [FromHeader]string username, [FromHeader]string password, [FromHeader]string companyId)
+        {
+            if (identity!=null && Services.ConnectionManager.IsIdentity(identity, out ClientConnection client))
             {
-                return Json(new { status = "success", message = "Logged in successfully" });
+                if (username == null || password == null || username.Length < 1 || password.Length < 1)
+                {
+                    return Json(new { status = "error", message = "Invalid Username or Password length" });
+                }
+                ClientConnection conn;
+
+                if (companyId == null) conn = await Services.Authentication.Login(client, username, password);
+                else conn = await Services.Authentication.Login(client, username, password, companyId);
+
+                bool successful_login = conn.cookies.Count > 0 && !conn.cookies.Any(x => x.Name == "id" && x.Value == "deleted");
+
+                if (successful_login)
+                {
+                    return Json(new { status = "success", message = "Logged in successfully" });
+                }
+                else
+                {
+                    return Json(new { status = "error", message = "You provided incorrect details" });
+                }
             }
-            else
-            {
-                return Json(new { status = "error", message = "You provided incorrect details" });
-            }
+            return Json(new { status = "error", message = "Your identity is invalid" });
         }
     }
 }
---
+++ HireHop-Login-Interface/Services/ConnectionManager.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HireHop_Login_Interface.Services
+{
+    public static class ConnectionManager
+    {
+        private static Dictionary<string, ClientConnection> tracked_connections = new Dictionary<string, ClientConnection>();
+
+
+        private static Random rnd = new Random();
+        private static string rndString(uint length = 32)
+        {
+            string s = "";
+            for (uint i = 0; i < length; i++) s += (char)rnd.Next(65,122);
+            return s.Replace("\\", "/");
+        }
+
+        public static string CreateClient()
+        {
+            string identifier = rndString();
+            tracked_connections.Add(identifier, new ClientConnection());
+            return identifier;
+        }
+
+        public static bool IsIdentity(string identity, out ClientConnection client)
+        {
+            if (tracked_connections.ContainsKey(identity))
+            {
+                client = tracked_connections[identity];
+                return true;
+            }
+            else
+            {
+                client = null;
+                return false;
+            }
+        }
+    }
+}
